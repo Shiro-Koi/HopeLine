5b35c2f9a028dada34a3b4521d207395fdba375c
Dyary Yassin <dyaryyasin165@gmail.com>
2023-06-23 02:42:22 +0300

    Added the map in flutter and mapbox integration

--- lib/map.dart
+++ lib/map.dart
@@ -1,55 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:hope_line/applicationConstants.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-
-class Map extends StatefulWidget {
+class MapView extends StatefulWidget {
   @override
-  _MapState createState() => _MapState();
+  _MapViewState createState() => _MapViewState();
 }
 
-class _MapState extends State<Map> {
+class _MapViewState extends State<MapView> {
   // Add your map-related variables and methods here
 
   @override
   Widget build(BuildContext context) {
-    // Build your map UI here
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Map'),
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 9.2,
       ),
-      body: Container(
-        // Add your map widget or custom map implementation here
-        child: Center(
-          child: Text('Map Widget'),
-          
-@override
-Widget build(BuildContext context) {
-  return FlutterMap(
-    options: MapOptions(
-      center: LatLng(51.509364, -0.128928),
-      zoom: 9.2,
-    ),
-    nonRotatedChildren: [
-      RichAttributionWidget(
-        attributions: [
-          TextSourceAttribution(
-            'OpenStreetMap contributors',
-            onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-          ),
-        ],
-      ),
-    ],
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'com.example.app',
-      ),
-    ],
-  );
-}
-        ),
-      ),
+      children: [
+        TileLayer(
+            urlTemplate:
+                "https://api.mapbox.com/styles/v1/dhruv25/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
+            additionalOptions: {
+              'mapStyleId': applicationConstants.mapBoxStyleId,
+              'accessToken': applicationConstants.mapBoxAccessToken,
+            }),
+      ],
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: const Color.fromARGB(255, 33, 32, 32),
+  //       title: const Text('Flutter MapBox'),
+  //     ),
+  //     body: Stack(
+  //       children: [
+  //         FlutterMap(
+  //           options: MapOptions(
+  //             minZoom: 5,
+  //             maxZoom: 18,
+  //             zoom: 13,
+  //             center: applicationConstants.myLocation,
+  //           ),
+  //           layers: [
+  //             TileLayerOptions(
+  //               urlTemplate:
+  //                   'https://api.mapbox.com/styles/v1/dyary017/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}',
+  //               additionalOptions: {
+  //                 'mapStyleId': applicationConstants.mapBoxStyleId,
+  //                 'accessToken': applicationConstants.mapBoxAccessToken,
+  //               },
+  //             ),
+  //           ],
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
