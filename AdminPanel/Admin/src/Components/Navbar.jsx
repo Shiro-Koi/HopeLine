6c2fa253af0fce92ff2d70ba567c8f5537e9cfbd
Dyary Yassin <dyaryyasin165@gmail.com>
2023-06-23 08:00:49 +0300

    Some more styling

--- AdminPanel/Admin/src/Components/Navbar.jsx
+++ AdminPanel/Admin/src/Components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
   return (
     <div className="bg-bg-secondary w-full flex justify-between items-center h-[15vh]">
       <div className="logo text-slate-950 ">HOPE LINE</div>
-      <ul>
+      <ul className="flex text-slate-950 px-5">
         <li>Admin Dashboard</li>
-        <li>Frequently Asked Questions</li>
+        <li className="px-5">Frequently Asked Questions</li>
         <li>Developers</li>
       </ul>
     </div>
