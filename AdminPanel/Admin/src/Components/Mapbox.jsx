545df3b5c79684ab914c9c62f163f7dc87ab1619
Dyary Yassin <dyaryyasin165@gmail.com>
2023-06-23 09:25:02 +0300

    Implemented Note Taking Functionality

--- AdminPanel/Admin/src/Components/Mapbox.jsx
+++ AdminPanel/Admin/src/Components/Mapbox.jsx
@@ -8,12 +8,13 @@ import Map from "react-map-gl";
 const Mapbox = () => {
   return (
     <Map
+      className=" rounded-2xl"
       initialViewState={{
         longitude: 45.4351,
         latitude: 35.5558,
         zoom: 9.7,
       }}
-      style={{ width: 800, height: 500 }}
+      style={{ width: 800, height: 500, borderRadius: 10 }}
       mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
       mapboxAccessToken="pk.eyJ1IjoiZHlhcnkwMTciLCJhIjoiY2wxaDhtams2MGJrcTNqbjJ5N2s2bTh5diJ9.cidFRjA1obU6y8MoJTy3RA"
     />
--- AdminPanel/Admin/src/pages/Home.jsx
+++ AdminPanel/Admin/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Mapbox from "../Components/Mapbox";
+import Note from "../Components/Note";
 
 const Home = () => {
   return (
     <div className="flex items-center justify-center flex-col bg-bg-main h-[80vh] m-0 p-0">
-      <Mapbox />
+      <div className="flex ">
+        <Mapbox className="grow" />
+        <Note />
+      </div>
       <div className="text-4xl"></div>
     </div>
   );
