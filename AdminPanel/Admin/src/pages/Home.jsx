d6bde14830efbe155ecd758bd0cf9d817fd6d039
Dyary Yassin <dyaryyasin165@gmail.com>
2023-06-23 07:16:29 +0300

    Setted up the Home Page and shared page configurat

--- AdminPanel/Admin/src/pages/Home.jsx
+++ AdminPanel/Admin/src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="bg-red-700">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, quae
+      adipisci? Illum obcaecati et, facere quam perferendis odio provident
+      assumenda rerum vel, ullam beatae quas. Mollitia odio magnam architecto
+      perspiciatis?
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
