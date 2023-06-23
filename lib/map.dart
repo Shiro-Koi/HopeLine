ed21295cf88a24173f02e46f5671a1159d12b3f8
Dyary Yassin <dyaryyasin165@gmail.com>
2023-06-23 03:27:33 +0300

    basic map impementation

--- lib/map.dart
+++ lib/map.dart
@@ -29,36 +29,4 @@ class _MapViewState extends State<MapView> {
       ],
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       backgroundColor: const Color.fromARGB(255, 33, 32, 32),
-  //       title: const Text('Flutter MapBox'),
-  //     ),
-  //     body: Stack(
-  //       children: [
-  //         FlutterMap(
-  //           options: MapOptions(
-  //             minZoom: 5,
-  //             maxZoom: 18,
-  //             zoom: 13,
-  //             center: applicationConstants.myLocation,
-  //           ),
-  //           layers: [
-  //             TileLayerOptions(
-  //               urlTemplate:
-  //                   'https://api.mapbox.com/styles/v1/dyary017/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}',
-  //               additionalOptions: {
-  //                 'mapStyleId': applicationConstants.mapBoxStyleId,
-  //                 'accessToken': applicationConstants.mapBoxAccessToken,
-  //               },
-  //             ),
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 }
