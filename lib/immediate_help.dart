57080106dbdead69c80c27e4c6d744f32d9beaf9
Dyary Yassin <dyaryyasin165@gmail.com>
2023-06-24 01:31:30 +0300

    Managed to get the user's precised location.

--- lib/immediate_help.dart
+++ lib/immediate_help.dart
@@ -3,21 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:hope_line/applicationConstants.dart';
 
 class ImmediateHelp extends StatelessWidget {
-  ImmediateHelp({Key? key}) : super(key: key) {
-    var firebase = applicationConstants.getFirebaseInstance();
-    var dataRef = firebase.ref("data");
-
-    var newRef = dataRef.push();
-    newRef.set({
-      "longitude": 0,
-      "latitude": 0,
-      "date_sent": DateTime.now().millisecondsSinceEpoch
-    });
-  }
-
+  ImmediateHelp({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
