755142e8c4ac948bc664e221f622d1dcb04c1723
Nini <asadfalah2@gmail.com>
2023-06-24 10:54:49 +0300

    fixing the initial screen and adding Firebase support to iOS

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,19 +6,17 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:url_launcher_web/url_launcher_web.dart';
-
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_database_web/firebase_database_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  UrlLauncherPlugin.registerWith(registrar);
-
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseDatabaseWeb.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
